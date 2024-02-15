2cbf84b273e71c5cb75bc60f0c7b22a9984b53b7
Marcelo <marc@Marcelos-MacBook-Air.local>
2024-02-15 14:18:11 -0300

    correcting the imgs adress

--- app/components/Newsletter/Newsletter.tsx
+++ app/components/Newsletter/Newsletter.tsx
@@ -18,8 +18,8 @@ const Newsletter = () => {
                                 <div className="relative text-white focus-within:text-white flex flex-row-reverse rounded-full pt-5 lg:pt-0">
                                     <input type="Email address" name="q" className="py-6 lg:py-8 text-sm md:text-lg w-full mx-3 text-black rounded-full pl-8 focus:outline-none focus:text-black" placeholder="Digite seu e-mail aqui." autoComplete="off" />
                                     <div className="absolute inset-y-0 right-0 flex items-center pr-6 pt-5 lg:pt-0">
-                                        <button type="submit" className="p-3 lg:p-5 focus:outline-none focus:shadow-outline bg-ultramarine hover:bg-midnightblue duration-150 ease-in-out rounded-full">
-                                            <Image src={'/assets/newsletter/send.svg'} alt="send-icon" width={30} height={30} />
+                                        <button type="submit" className="p-3 lg:p-5 focus:outline-none focus:shadow-outline bg-epec-blue hover:bg-midnightblue duration-150 ease-in-out rounded-full">
+                                            <Image src={'./assets/newsletter/send.svg'} alt="send-icon" width={30} height={30} />
                                         </button>
                                     </div>
                                 </div>
