6bacbb3a7c5023b26fef463ee1878b2d0905c848
Marcelo <marc@Marcelos-MacBook-Air.local>
2024-02-15 13:31:35 -0300

    correcting the andress of the images

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -59,12 +59,12 @@ const Navbar = () => {
                             <div className="flex flex-shrink-0 items-center">
                                 <img
                                     className="block h-12 w-40 lg:hidden"
-                                    src={'/assets/logo/logo.svg'}
+                                    src={'./assets/logo/logo.svg'}
                                     alt="dsign-logo"
                                 />
                                 <img
                                     className="hidden h-full  w-40 lg:block"
-                                    src={'/assets/logo/logo.svg'}
+                                    src={'./assets/logo/logo.svg'}
                                     alt="dsign-logo"
                                 />
                             </div>
