06a31dd544b25bac9cb895b9b394d435bc624842
Marcelo <marc@Marcelos-MacBook-Air.local>
2024-02-15 21:20:41 -0300

    adicionando next-optimized-images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /**
  * @type {import('next').NextConfig}
  */
+const withOptimizedImages = require('next-optimized-images');
+
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-  }
-   
-  module.exports = nextConfig
+  output: 'export', // ou qualquer outra configuração de saída que você precise
+  images: {
+    unoptimized: true, // Garante que as imagens não otimizadas serão usadas como fallback
+  },
+};
+
+module.exports = withOptimizedImages(nextConfig);
