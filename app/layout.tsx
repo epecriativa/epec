c2ee314544318b2904af5e0afa20d4733c39fa36
abreu pro rap <abreuprorap@gmail.com>
2026-04-24 15:31:41 -0300

    feat: redesign home banner, atualizações de fontes e integração do botão do WhatsApp

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import '@/node_modules/react-modal-video/scss/modal-video.scss';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/Footer';
+import Image from 'next/image';
 
 
 export const metadata = {
@@ -20,6 +21,19 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <a 
+          href="https://wa.me/5581991337935" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="fixed bottom-6 right-6 z-50 hover:scale-110 transition-transform duration-300"
+        >
+          <Image 
+            src="/whatsapp popup icon.svg" 
+            alt="WhatsApp Contato" 
+            width={60} 
+            height={60} 
+          />
+        </a>
       </body>
     </html>
   )
