efcf929775a2c62c387cbed6de6435322c25601d
EPEC Criativa <epecriativa@gmail.com>
2026-04-25 16:12:58 -0300

    Ajuste de posição Entrar e setas mobile conforme demarcação

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -91,7 +91,7 @@ const Navbar = () => {
                         {/* SIGNIN & MOBILE MENU */}
 
                         <div className="flex items-center lg:gap-0">
-                            <div className="mr-8">
+                            <div className="mr-14">
                                 <Signdialog />
                             </div>
                             
